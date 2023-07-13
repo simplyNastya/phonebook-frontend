92e1490e47495efc8d79e7ae139cf3af8eac1362
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-07-14 00:12:58 +0200

    Added styles
    Refactored code

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import { login } from "../../redux/auth/auth-operations";
 import LoginForm from "../../components/LoginForm/LoginForm";
 
-// import styles from './loginPage.module.css';
+import styles from "./loginPage.module.css";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -10,12 +10,12 @@ const LoginPage = () => {
   const onLogin = (data) => dispatch(login(data));
 
   return (
-    <>
-      <h1>
-        Nice to see you again, just log in and we will continue our trip :)
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>
+        Nice to see you again, just sign in and we will continue our trip :)
       </h1>
       <LoginForm onSubmit={onLogin} />
-    </>
+    </div>
   );
 };
 
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -2,16 +2,20 @@ import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/auth-operations";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 
+import styles from "./registerPage.module.css";
+
 const RegisterPage = () => {
   const dispatch = useDispatch();
 
   const onRegister = (data) => dispatch(register(data));
 
   return (
-    <>
-      <h1>Let`s start from registration :)</h1>
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>
+        Please fill in the forms so we can start!
+      </h1>
       <RegisterForm onSubmit={onRegister} />
-    </>
+    </div>
   );
 };
 
