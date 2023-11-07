8c037d199b804274ef1ed372c42f8de26bb09df7
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-11-07 14:42:15 +0100

    Add responsible markup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 import { useSelector } from "react-redux";
 import { RiContactsBookLine } from "react-icons/ri";
 
+import { NavLink } from "react-router-dom";
+import useWindowSize from "../../services/hooks/useWindowSize";
 import NavbarAuth from "./NavbarAuth/NavbarAuth";
 import NavbarUser from "./NavbarUser/NavbarUser";
 import { isUserLogin } from "../../redux/auth/auth-selectors";
 
 import styles from "./navbar.module.css";
-import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const isLogin = useSelector(isUserLogin);
+  const { width } = useWindowSize();
 
   return (
     <>
@@ -18,7 +20,9 @@ const Navbar = () => {
           <nav className={styles.nav}>
             <NavLink to="/" className={styles.link}>
               <RiContactsBookLine className={styles.logo} />
-              <p className={styles.logoText}>PhoneBook</p>
+              {width <= 527 ? null : (
+                <p className={styles.logoText}>PhoneBook</p>
+              )}
             </NavLink>
             {!isLogin && <NavbarAuth />}
             {isLogin && (
--- src/components/Navbar/NavbarUser/NavbarUser.jsx
+++ src/components/Navbar/NavbarUser/NavbarUser.jsx
@@ -1,11 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
+import useWindowSize from "../../../services/hooks/useWindowSize";
 import { getUser } from "../../../redux/auth/auth-selectors";
 import { logout } from "../../../redux/auth/auth-operations";
 import Button from "../../Button/Button";
 import styles from "./navbarUser.module.css";
 
+import { BiLogOut } from "react-icons/bi";
+
 const NavbarUser = () => {
   const { name } = useSelector(getUser);
+  const { width } = useWindowSize();
 
   const dispatch = useDispatch();
   const onLogout = () => {
@@ -16,7 +20,7 @@ const NavbarUser = () => {
     <div className={styles.wrapper}>
       <p className={styles.userName}>{name}</p>
       <Button className={styles.logoutBtn} onClick={onLogout}>
-        Logout
+        {width <= 527 ? <BiLogOut /> : `Logout`}
       </Button>
     </div>
   );
